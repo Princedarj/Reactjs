371e27939aa4ea9f08ec3368415120232cc78af1
Prince Darji <prince@example.com>
2026-04-06 14:47:18 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id='dad'>
+        <h1 id='child-1'>Hello, World!</h1>
+        <h3 id='child-2'>Welcome to React!</h3>
+      </div>
+
+      <div id="chacha">
+        <p>This is the chacha div.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
