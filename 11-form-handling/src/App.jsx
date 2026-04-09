e5ba40b157a6ba63caa5a1202b64331238806f3b
Prince Darji <prince@example.com>
2026-04-09 17:24:19 +0530

    Added new data

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,21 @@
+
+const App = () => {
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    console.log('Form submited');
+  }
+
+  return (
+    <div>
+    <form  onSubmit={(e) => {
+      submitHandler(e)
+      }}>
+        <input type="text" placeholder="Enter Your Name" />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
