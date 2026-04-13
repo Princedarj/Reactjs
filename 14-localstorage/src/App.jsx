05269193f9ecac6ad645ad8ef0a132e1cd58ef75
Prince Darji <prince@example.com>
2026-04-13 11:44:18 +0530

    Local Storage Done

--- 14-localstorage/src/App.jsx
+++ 14-localstorage/src/App.jsx
@@ -1,9 +1,45 @@
 
+// const App = () => {
+
+//   const First = {
+//     User : 'Prince',
+//     age :  21,
+//     City: 'Ahmedabad'
+//   }
+   
+//   localStorage.setItem('First',JSON.stringify(First))
+
+//   const first = JSON.parse(localStorage.getItem('First'))
+
+//   console.log("First: ", first);
+
+
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 const App = () => {
 
- const prince = localStorage.getItem('prince')
-  console.log(prince);
   
+  const User = {
+    User : 'JP',
+    age :  21,
+    City: 'BanashKantha'
+  }
+   
+  localStorage.setItem('JP',JSON.stringify(User))
+
+
+  const first = JSON.parse(localStorage.getItem('user'))
+
+  console.log("user: ", first);
+
   return (
     <div>
       
